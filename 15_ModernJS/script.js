c54e2791fdb8a7bd6f13c74dab4026a11871319b
Filip Kocian <filkocian@gmail.com>
2022-12-20 18:16:33 +0100

    npm, parcel and babel lectures

--- 15_ModernJS/script.js
+++ 15_ModernJS/script.js
@@ -20,3 +20,49 @@ add('product', 5);
 add('product', 5);
 
 console.log(ShoppingCart.cart);
+
+// console.log('start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+
+// console.log(data);
+// console.log('end fetching');
+
+async function getLastPost() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+}
+
+//! Doesn't work
+// const lastPost = getLastPost();
+// console.log('🚀 ~ file: script.js ~ line 40 ~ lastPost', lastPost);
+
+// * Correct way
+// const lastPost = await getLastPost();
+// console.log('🚀 ~ file: script.js ~ line 43 ~ lastPost', lastPost);
+
+// * npm basics
+
+import lodash from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateDeepClone = lodash.cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateDeepClone); // No change in the object
+
+// HMR
+if (module.hot) {
+  module.hot.accept();
+}
+
+import 'core-js/stable/array/find';
+import 'regenerator-runtime/runtime';
--- 15_ModernJS/shoppingCart.js
+++ 15_ModernJS/shoppingCart.js
@@ -1,5 +1,11 @@
 // Exporting Module
 
+// Blocking code (top level await)
+// console.log('start fetching');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+
+// console.log('end fetching');
+
 const shippingCost = 10;
 export const cart = [];
 
